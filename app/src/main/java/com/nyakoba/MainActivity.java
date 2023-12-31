b07cb74e38bb46f2ec08335bbbf5fa410336d78d
Dennis Muthomi Mutegi <DMUTEGI@safaricom.co.ke>
2023-12-31 14:07:32 +0300

    Initial login page

--- app/src/main/java/com/nyakoba/MainActivity.java
+++ app/src/main/java/com/nyakoba/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nyakoba;
 
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+
+        Toast.makeText(MainActivity.this, "Hello there",Toast.LENGTH_SHORT).show();
     }
 }

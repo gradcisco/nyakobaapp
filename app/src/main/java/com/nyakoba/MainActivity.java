6ffa0a5b58480fbe30cc8707b5642230a9e2436c
Dennis Muthomi Mutegi <DMUTEGI@safaricom.co.ke>
2023-12-31 16:10:28 +0300

    Initial login page

--- app/src/main/java/com/nyakoba/MainActivity.java
+++ app/src/main/java/com/nyakoba/MainActivity.java
@@ -5,6 +5,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.nyakoba.model.LoginRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
         String username = editTextUsername.getText().toString();
         String password = editTextPassword.getText().toString();
-
         Toast.makeText(MainActivity.this, "Hello there Username = " + username + " and password = " + password,Toast.LENGTH_SHORT).show();
+
+
+        /*LoginRequest loginRequest = LoginRequest
+                .builder()
+                .username(username)
+                .password(password)
+                .build();*/
+
+        LoginRequest loginRequest1 = new LoginRequest();
+        loginRequest1.setUsername(username);
+        loginRequest1.setPassword(password);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://webhook.site/a05a1418-856f-45d9-b577-2cfb2f16ab1a";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<LoginRequest> entity = new HttpEntity<LoginRequest>(loginRequest1 , headers);
+        String result = restTemplate.exchange(url, HttpMethod.POST , entity, String.class).getBody();
+
+        Toast.makeText(MainActivity.this, "result",Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
---
+++ app/src/main/java/com/nyakoba/model/LoginResponse.java
@@ -0,0 +1,7 @@
+package com.nyakoba.model;
+
+
+public class LoginResponse {
+    private String status;
+    private String description;
+}

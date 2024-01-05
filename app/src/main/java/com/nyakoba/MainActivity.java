c21e65849c23ad7a9ffe3cbac2566ccad962d41b
Dennis Muthomi Mutegi <DMUTEGI@safaricom.co.ke>
2024-01-05 08:02:26 +0300

    grok

--- app/src/main/java/com/nyakoba/MainActivity.java
+++ app/src/main/java/com/nyakoba/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     //KB0430215
     //0944563
    // private String url = "http://192.168.100.45:8080/login";
-    private String url = "https://3758-41-90-64-183.ngrok-free.app/login";
+    private String url = "https://4661-41-90-68-214.ngrok-free.app/login";
 
 
     private EditText editTextUsername, editTextPassword;
